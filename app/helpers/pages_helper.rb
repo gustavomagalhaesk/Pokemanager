23a412a8f36dd3806f8aa16a738b4f2d11bf1c20
Gustavo Magalhaes <gustavomagalhaesk@gmail.com>
2026-04-09 11:33:51 -0400

    currency on pokeshop

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+
+  def convert_price(price, to_price)
+    if to_price == "real"
+      price = CurrencyService.convert("USD", "BRL", price)
+      return "R$ #{price.round(2)}"
+    elsif to_price == "iene"
+      price = CurrencyService.convert("USD", "JPY", price)
+      return "¥ #{price.round(0)}"
+    else 
+      return "$ #{price.round(2)}"
+    end
+  end
 end
